e4f1378d8561949b1e1d6725b7cc310d19735c63
Hazel Wu <tinazx056@gmail.com>
2019-01-12 20:54:04 +0800

    Initialize commit project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <appQuoteGrid></appQuoteGrid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './component/QuoteGrid.vue';
+    export default {
+        components: {
+            appQuoteGrid: QuoteGrid
+        },
+        data(){
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
